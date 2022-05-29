f376d3fd8903039c30b901043cb65501c7a95434
Romantic <dupyi0912@gmail.com>
2022-05-29 13:57:57 +0800

    fate: add eslint and prettierrc

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        cookieDomainRewrite: "",
+        secure: false
+      }
+    }
+  },
   plugins: [react()]
 })
