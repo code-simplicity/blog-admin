5e9890c9ca27e25461f66ca8e8a8682e498a84d8
Romantic <dupyi0912@gmail.com>
2022-05-30 00:03:34 +0800

    feat: 开启imp自动导入组件的功能

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginImp from 'vite-plugin-imp'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   server: {
+    host: true,
     port: 3080,
     proxy: {
       '/api': {
@@ -14,5 +18,30 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react()]
+  plugins: [react(), vitePluginImp(
+    {
+      libList: [
+        {
+          libName: 'lodash',
+          libDirectory: '',
+          camel2DashComponentName: false
+        },
+        {
+          libName: 'antd',
+          style(name) {
+            // use less
+            return `antd/es/${name}/style/index.js`
+          }
+        },
+      ]
+    }
+  )],
+  css: {
+    preprocessorOptions: {
+      less: {
+        // 支持内联 JavaScript
+        javascriptEnabled: true,
+      },
+    },
+  },
 })
